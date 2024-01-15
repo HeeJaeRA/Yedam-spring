35568b70d7afc4ab1d6f4f472c5c08c1b8a0e8be
heejaeRA <rhj7369@nate.com>
2024-01-15 10:26:53 +0900

    demo

--- demo00/src/main/java/com/yedam/app/emp/mapper/EmpMapper.java
+++ demo00/src/main/java/com/yedam/app/emp/mapper/EmpMapper.java
@@ -1,5 +1,9 @@
 package com.yedam.app.emp.mapper;
 
-public interface EmpMapper {
+import java.util.List;
 
+import com.yedam.app.emp.service.EmpVO;
+
+public interface EmpMapper {
+	public List<EmpVO> selectEmpList();
 }
